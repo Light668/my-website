7b795a169867060bba94b68abaf99434f877a7a1
unknown <timo.radtke@wwu.de>
2020-02-09 14:33:39 +0100

    updated skillset.component, reworked footer.

--- src/app/Routing/aboutme/aboutme.component.ts
+++ src/app/Routing/aboutme/aboutme.component.ts
@@ -7,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutmeComponent implements OnInit {
 
+  private selectedStep: number = 0;
+
   constructor() { }
 
   ngOnInit() {
 
   }
 
+  animationEnded(event) {
+
+    let id: string = "cdk-step-label-0-" + event.selectedIndex;
+    console.log(event);
+    console.log(id);
+    let element = document.getElementById(id);
+    console.log(element);
+    setTimeout(() => { element.scrollIntoView(); }, 200);
+    let ypos = element.clientHeight;
+    console.log(ypos);
+  }
 }
