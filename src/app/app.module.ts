892d6c1194c2ab3bb301c6bb1e0639463c1ab030
Light668 <timo.radtke@wwu.de>
2020-05-18 17:20:12 +0200

    updated angular materials and cdk to version 9.2.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 import { InterestsComponent } from './Routing/interests/interests.component';
 import { SkillsetComponent } from './Routing/skillset/skillset.component';
 import { AboutmeComponent } from './Routing/aboutme/aboutme.component';
