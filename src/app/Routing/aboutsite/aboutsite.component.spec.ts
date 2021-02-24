2cbae8705fded17b1bf4d22f8d6e2aafe14ee29e
Light668 <timo.radtke@wwu.de>
2021-02-24 13:06:43 +0100

    updated to angular 11 part1

--- src/app/Routing/aboutsite/aboutsite.component.spec.ts
+++ src/app/Routing/aboutsite/aboutsite.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AboutsiteComponent } from './aboutsite.component';
 
@@ -6,7 +6,7 @@ describe('AboutsiteComponent', () => {
   let component: AboutsiteComponent;
   let fixture: ComponentFixture<AboutsiteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AboutsiteComponent ]
     })
