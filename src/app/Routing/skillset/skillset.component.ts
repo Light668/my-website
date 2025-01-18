35b6b72b13ead70106bf889d9c82fa888e2e0255
Light668 <timoradtke2@gmail.com>
2025-01-18 22:09:51 +0100

    Recovered SkillsetComponent with new Angular CDK.

--- src/app/Routing/skillset/skillset.component.ts
+++ src/app/Routing/skillset/skillset.component.ts
@@ -3,21 +3,27 @@ import { ScrolledView, ViewServiceService } from '../../service/view-service.ser
 import { MatCardModule } from '@angular/material/card';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { FooterComponent } from 'src/app/footer/footer.component';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-skillset',
   templateUrl: './skillset.component.html',
   styleUrls: ['./skillset.component.css'],
   standalone: true,
-  imports: [MatCardModule, CdkAccordionModule, FooterComponent]
+  imports: [MatCardModule, CdkAccordionModule, MatProgressBarModule,
+    MatExpansionModule, FooterComponent, CommonModule]
 })
 export class SkillsetComponent implements OnInit {
 
   value = 0;
   intervalId;
+  public isMobileView: boolean;
+  public isWebView: boolean;
 
-  constructor(private scrollViewService: ViewServiceService) {
+  constructor(private scrollViewService: ViewServiceService, public responsive: BreakpointObserver) {
 
   }
 
@@ -32,6 +38,23 @@ export class SkillsetComponent implements OnInit {
         }, 200);
       }
     });
+
+    this.responsive.observe(Breakpoints.Handset)
+      .subscribe((result: BreakpointState) => {
+
+        if (result.matches) {
+          console.log("screens matches Handset");
+          this.isMobileView = true;
+        } else {
+          console.log("screen does not match Handset");
+          this.isWebView = true;
+        }
+
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.responsive.ngOnDestroy(); 
   }
 
   valueCalc(value: number) {
